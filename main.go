d265d8459a87e4f0b922a26897a557a791fb9233
Alexey Sytchenko <hirooshi998@gmail.com>
2024-09-29 15:31:11 +0300

    feature: new main.go

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+// Структуры для парсинга FB2
+type FictionBook struct {
+	XMLName  xml.Name `xml:"FictionBook"`
+	Author   Author   `xml:"description>title-info>author"`
+	Sequence Sequence `xml:"description>title-info>sequence"`
+}
+
+type Author struct {
+	FirstName string `xml:"first-name"`
+	LastName  string `xml:"last-name"`
+}
+
+type Sequence struct {
+	Name   string `xml:"name,attr"`
+	Number string `xml:"number,attr"`
+}
+
+func main() {
+	// Чтение файла fb2
+	filePath := "book.fb2"
+	xmlFile, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Ошибка открытия файла:", err)
+		return
+	}
+	defer xmlFile.Close()
+
+	// Чтение содержимого файла
+	byteValue, _ := ioutil.ReadAll(xmlFile)
+
+	// Парсинг XML
+	var book FictionBook
+	err = xml.Unmarshal(byteValue, &book)
+	if err != nil {
+		fmt.Println("Ошибка парсинга XML:", err)
+		return
+	}
+
+	// Формирование структуры автора для JSON
+	result := map[string]interface{}{
+		"author": map[string]string{
+			"first_name": book.Author.FirstName,
+			"last_name":  book.Author.LastName,
+		},
+		"sequence": map[string]string{
+			"name":   book.Sequence.Name,
+			"number": book.Sequence.Number,
+		},
+	}
+
+	// Преобразование в JSON
+	resultJSON, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		fmt.Println("Ошибка создания JSON:", err)
+		return
+	}
+
+	// Вывод JSON
+	fmt.Println(string(resultJSON))
+}
